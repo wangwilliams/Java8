6b8f37295f00ddd98f2ca0e25d749f4dadf69295
WangWilliams <1179574672@qq.com>
2020-03-19 18:41:34 +0800

    java8 lambda表达式

--- src/com/wangyunpeng/chapter1/LambdaTest.java
+++ src/com/wangyunpeng/chapter1/LambdaTest.java
@@ -1,10 +1,26 @@
 package com.wangyunpeng.chapter1;
 
+import java.util.Comparator;
+
 /**
  * Lambda 表达式的使用例子
  */
 public class LambdaTest {
-    public void test1() {
+    public static void main(String[] args) {
+        Comparator<Integer> comparable = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 > o2) ? o1 : o2;
+            }
+        };
+        Comparator<Integer> integerComparator = (o1,o2)->{
+            return (o1 > o2) ? o1 : o2;
+        };
 
-   }
+        Comparator<Integer> integerComparator1 = Integer::compare;
+
+        System.out.println(comparable.compare(1,2));
+        System.out.println(integerComparator.compare(1,2));
+        System.out.println(integerComparator1.compare(3,1));
+    }
 }
